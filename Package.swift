1c090b0ec32db6befb116b0f5ef834c1f29942c9
Jared Payne <jaredmpayne@gmail.com>
2020-11-16 04:11:45 -0500

    Add Drools CLI executable product and target

--- Package.swift
+++ Package.swift
@@ -7,22 +7,31 @@ let package = Package(
     name: "DroolsAnalysis",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .executable(
+            name: "Drools",
+            targets: ["Drools"]
+        ),
         .library(
             name: "DroolsAnalysis",
-            targets: ["DroolsAnalysis"]),
+            targets: ["DroolsAnalysis"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jaredmpayne/SwiftArg.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
+            name: "Drools",
+            dependencies: ["DroolsAnalysis", "SwiftArg"]
+        ),
+        .target(
             name: "DroolsAnalysis",
             dependencies: []),
         .testTarget(
             name: "DroolsAnalysisTests",
-            dependencies: ["DroolsAnalysis"]),
+            dependencies: ["DroolsAnalysis"])
     ]
 )
